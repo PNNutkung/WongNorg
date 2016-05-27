87907c4ed7890102c7b65788788bfbc1b2396df5
Pipatpol Tanavongchinda <pipatpol.t@ku.th>
2016-05-27 16:35:17 +0700

    Now can random restaurant

--- app/src/main/java/th/in/pnnutkung/wongnorg/activity/RandomRestaurant.java
+++ app/src/main/java/th/in/pnnutkung/wongnorg/activity/RandomRestaurant.java
@@ -2,20 +2,41 @@ package th.in.pnnutkung.wongnorg.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.List;
 
 import th.in.pnnutkung.wongnorg.R;
 import th.in.pnnutkung.wongnorg.model.Restaurant;
+import th.in.pnnutkung.wongnorg.model.Storage;
 
 public class RandomRestaurant extends AppCompatActivity {
     Restaurant recentRestaurant;
+    List<Restaurant> resList;
+    int currentRandom = -1,random;
+    TextView resName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_random_restaurant);
-
+        if(resList == null) {
+            Log.e("resList","Create new");
+            resList = Storage.getInstance().getRestaurantsList();
+        }
     }
 
-    public void randomRestaurant(){
-
+    public void randomRestaurant(View view){
+        random = (int)Math.floor(Math.random()*resList.size());
+        Log.e("Random",String.format("%d",random));
+        if(currentRandom == random){
+            randomRestaurant(view);
+        } else {
+            currentRandom = random;
+            recentRestaurant = resList.get(random);
+            resName = (TextView)findViewById(R.id.randomRestaurantName);
+            resName.setText(recentRestaurant.getName());
+        }
     }
 }
